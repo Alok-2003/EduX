d357568237b503fb9a677465ba5b2ca6be6a8fe3
Alok-2003 <alok953280@gmail.com>
2024-12-31 12:22:21 +0530

    Enterprise page done

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,36 @@
 import { LoginButton, useOCAuth } from '@opencampus/ocid-connect-js';
 import { Link } from 'react-router-dom';
+import { useFirebase } from '../contexts/FirebaseContext';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-    const { authState, OCId } = useOCAuth();
+    const { authState, ethAddress, OCId } = useOCAuth();
+    const { fetchAllUsers } = useFirebase();
+    const [role, setRole] = useState<string | null>(null);
+
+    // useEffect(() => {
+    //     const fetchUserRole = async () => {
+    //         try {
+    //             if (ethAddress) {
+    //                 const users = await fetchAllUsers();
+    //                 if (!users || users.length === 0) {
+    //                     return;
+    //                 }
+    //                 const matchedUser = users.find(
+    //                     (user: any) => user.id?.toLowerCase() === ethAddress.toLowerCase()
+    //                 );
+    //                 if (matchedUser) {
+    //                     setRole(matchedUser.role);
+    //                 }
+    //             }
+    //         } catch (error) {
+    //             console.error("Error fetching user role:", error);
+    //         }
+    //     };
+    //     fetchUserRole();
+    // }, [ethAddress, fetchAllUsers]);
+    
+    
 
     return (
         <header className="bg-dark text-white py-4 px-4 sm:px-6 lg:px-8">
@@ -13,8 +41,12 @@ export default function Header() {
                 <nav className="hidden md:flex space-x-10">
                     <Link to="/" className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Home</Link>
                     <Link to="/learn" className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Learn</Link>
-                    <Link to="/jobs" className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Jobs</Link>
-                    <Link to="/enterprise" className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Enterprise</Link>
+                    {role !== 'enterprise' && (
+                        <Link to="/jobs" className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Jobs</Link>
+                    )}
+                    {role !== 'developer' && (
+                        <Link to="/enterprise" className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Enterprise</Link>
+                    )}
                     <Link to={'https://edux.gitbook.io/edux-docs'} className="hover:text-primary-light px-3 py-2 rounded-md transition-colors duration-200">Docs</Link>
                 </nav>
                 <div className="relative z-50">
