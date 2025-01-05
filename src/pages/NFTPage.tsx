c5321ca52764adcc8a21227cf403c39ebaf375f1
Aryan Zutshi <122102323+Aryanzutshi@users.noreply.github.com>
2025-01-05 11:47:34 +0530

    Update NFTPage.tsx

--- src/pages/NFTPage.tsx
+++ src/pages/NFTPage.tsx
@@ -7,13 +7,13 @@ import {
   import { ThirdwebProvider } from "thirdweb/react";
   
 const NFTPage = () => {
-    const client = createThirdwebClient({
+    const clientConfig = createThirdwebClient({
         clientId: "f518bd738fb261c709667a04f696d0da",
       });
       
       // connect to your contract
     const contract = getContract({
-        client,
+        clientConfig,
         chain: defineChain(656476),
         address: "0xC2F4BF524fDe0c295D4dc9429744cc03aa132758",
       });
